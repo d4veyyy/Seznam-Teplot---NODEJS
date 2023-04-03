07548f1ee008a56c5d37e1887f640823bca2c588
d4veyyy <benesdavidd@gmail.com>
2023-04-03 21:51:59 +0200

    Teploty

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-/* Připojení modulu frameworku Express */
 const express = require("express");
-
-/* Vytvoření základního objektu serverové aplikace */
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const csvtojson = require('csvtojson');
+const fs = require("fs");
+const path = require("path");
 const app = express();
-/* Nastavení portu, na němž bude spuštěný server naslouchat */
 const port = 3000;
 
-/* Spuštění webového serveru */
-app.listen(port, () =>{console.log(`Server naslouchá na portu ${port}`);
+app.use(express.static("public"));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(port, () => {
+    console.log(`Server naslouchá na portu ${port}`);
 });
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.post('/savedata', urlencodedParser, (req, res) => {
+    let date = moment().format('YYYY-MM-DD');
+    let str = `"${req.body.teplota}","${req.body.pocasi}","${date}","${req.body.datum}"\n`;
+    fs.appendFile(path.join(__dirname, 'data/teploty.csv'), str, function (err) {
+        if (err) {
+            console.error(err);
+            return res.status(400).json({
+                success: false,
+                message: "Nastala chyba během ukládání souboru"
+            });
+        }
+    });
+    res.redirect(301, '/');
+});
+
+
+app.get("/teploty", (req, res) => { 
+    csvtojson({headers:['teplota', 'pocasi', 'datum']}).fromFile(path.join(__dirname, 'data/teploty.csv')) 
+    .then(data => {  
+      res.render('index', {nadpis: "Teploty", teploty: data}); 
+    }) 
+    .catch(err => { 
+      console.log(err); 
+      res.render('error', {nadpis: "Chyba v aplikaci", chyba: err}); 
+    }); 
+  });
+  
